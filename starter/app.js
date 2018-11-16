617cf80d07cadb8345feb3ddf92dadfdc2211eae
david11yf29 <david11yf29@gmail.com>
2018-11-17 02:13:47 +0800

    Updated

--- starter/app.js
+++ starter/app.js
@@ -6,4 +6,30 @@ var budgetController = (function(){
         return x + a;
     }
     
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+    
 })();
+
+
+var UIController = (function() {
+    
+    // some code
+    
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var z = budgetCtrl.publicTest(5);
+    
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+    
+})(budgetController, UIController);
