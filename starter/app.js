92140af7abd8371a8d3304414397859c5b6a4291
david11yf29 <david11yf29@gmail.com>
2018-11-17 09:48:18 +0800

    Updated

--- starter/app.js
+++ starter/app.js
@@ -1,35 +1,21 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function(){
     
-    var x = 23;
-    
-    var add  = function(a) {
-        return x + a;
-    }
-    
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    //some code
     
 })();
 
-
+// UI CONTROLLER
 var UIController = (function() {
     
     // some code
     
 })();
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
-    var z = budgetCtrl.publicTest(5);
     
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
-    }
     
 })(budgetController, UIController);
